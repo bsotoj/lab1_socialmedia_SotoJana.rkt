b203f89475430c96aa6e00798a6e618ed08ece3a
Bastian Soto <bastian.soto.j@usach.cl>
2021-04-25 18:44:49 -0400

    contructor TDA SocialNetwork

--- TDA_SocialNetwork.rkt
+++ TDA_SocialNetwork.rkt
@@ -4,8 +4,8 @@
 ;Constructor TDA Date
 ;Dom: Dia <number> X mes <number> X año <number>
 ;Rec: TDA Date
-(define Date(lambda(dd mm yyyy)(
-                                cond [(Date? dd mm yyyy)(list dd mm yyyy)]
+(define date(lambda(dd mm yyyy)(
+                                cond [(date? dd mm yyyy)(list dd mm yyyy)]
                                       [else '()]                            
                                 
                                 )
@@ -14,9 +14,24 @@
 ;Pertenencia
 ;Dom: Dia <number> X mes <number> X año <number>
 ;Rec: Bool
-(define Date? (lambda (dd mm yyyy)(
+(define date? (lambda (dd mm yyyy)(
                                    cond [(and(number? dd)(number? mm)(number? yyyy)(list dd mm yyyy))]
 
                                          [else #f])               )
                 )
   
+
+;Funcion encryptFn
+;funcion que permite encriptar/desencriptar un mensaje
+;Dom: String
+;Rec: String
+(define encryptFn (lambda (s) (list->string (reverse (string->list s)))))
+
+;TDA SocialNetwork
+;Constructor
+;Dom:String X Date X EncryptFunction X DecryptFunction
+;Rec:TDA SocialNetwork
+
+(define socialnetwork (lambda(name date encryptFunction decryptFunction)
+  (list name date encryptFunction decryptFunction )
+  )) 
